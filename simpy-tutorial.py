464d8d049ca54adfbc6694800b3848d8290d6856
Anderson Pontes Batalha <andersonpbatalha@gmail.com>
2017-10-20 21:28:45 -0200

    Commit inicial

--- simpy-tutorial.py
+++ simpy-tutorial.py
@@ -6,5 +6,5 @@
 import random # módulo gerador de números aleatórios
 import simpy # biblioteca de simulação
 
-random.seed(1000) #semente para o gerador de números aleatórios, garante a sequência de números geradoa será sempre a mesma
+random.seed(1000) #semente para o gerador de números aleatórios, garante a sequência de números gerados será sempre a mesma
 env = simpy.Environment() # variável que representa o ambiente de simulação do SimPy
