efce2289a3dc801c6454f1f4d6a316c3d74824e8
AndersonBatalha <andersonpbatalha@gmail.com>
2017-10-24 23:26:44 -0200

    Commit inicial

--- simulacao-simpy.py
+++ simulacao-simpy.py
@@ -4,15 +4,6 @@
 import random # gera números aleatórios
 import simpy # biblioteca de simulação
 
-"""
-Probabilidades
-
-tempos_servico = {"0-5":35, "5-10":19, "10-15":19, "15-20":13, "20-25":3, "25-30":7, "30-35":1, "35-40":2, "40-45":1}
-
-tempo = {'0-5':random.uniform(0,5), '30-35':random.uniform(30,35), '5-10':random.uniform(5,10), '15-20':random.uniform(15,20), '35-40':random.uniform(35,40), '10-15':random.uniform(10,15), '20-25':random.uniform(20,25), '25-30':random.uniform(25,30), '40-45':random.uniform(40,45)}
-
-"""
-
 class Simulacao(object):
 	# o construtor da classe cria o ambiente de desenvolvimento e o recurso a ser utilizado, no caso o servidor
 	# o usuário deve passar como parâmetro apenas o tempo de simulação desejado
@@ -20,52 +11,53 @@ class Simulacao(object):
 		self.tempoSimulacao = tempoSimulacao #tempo em que a simulação é realizada
 		self.env = simpy.Environment() #variavel que representa o ambiente de simulação criado pelo SimPy
 		self.servidor = simpy.Resource(self.env, capacity=2) #variavel que representa o recurso a ser ocupado
-		self.env.process(self.chegadaEntidades(2))
+		self.env.process(self.chegadaClientes()) #env.process informa ao SimPy que a função chegadaClientes faz parte do processo de simulação
 		self.env.run(until=tempoSimulacao)
 
 	def __str__(self):
 		return "Simulação de um sistema com dois servidores"
 	
 	def tempoChegada(self, probabilidades = {}, lista = []):
-		"""Este método da classe gera um tempo de chegadas de acordo com a tabela 1 (ver descrição da atividade)
+		#Este método da classe gera um tempo de chegadas de acordo com a tabela 1 (ver descrição da atividade)
 		
-		Dicionário: pares no formato chave-valor, onde a chave representa a classe, e o valor é uma lista contendo a probabilidade(inteiro), e o método random.uniform, para gerar o tempo de chegada
-		
-		Como estamos trabalhando com porcentagem, se, por exemplo a probabilidade de ocorrência é de 39%, então são 39 valores iguais dentro de um vetor de 100 posições
-		
-		Criei uma lista vazia, para cada par chave-valor do dicionário, inseri o valor da chave em outro array, multiplicado por sua probabilidade
-		"""
-		self.probabilidades = {"0-5":[35, random.uniform(0,5)], "5-10":[19, random.uniform(5,10)], "10-15":[19, random.uniform(10,15)], "15-20":[13, random.uniform(15,20)], "20-25":[3, random.uniform(20,25)], "25-30":[7, random.uniform(25,30)], "30-35":[1, random.uniform(30,35)], "35-40":[2, random.uniform(0,5)], "40-45":[1, random.uniform(0,5)]}
+		# Um dicionário representa com pares no formato chave-valor, onde a chave representa a classe, e o valor é uma lista contendo a probabilidade(inteiro), e o método random.uniform, para gerar valores aleatórios de ponto flutuante
+
+		self.probabilidades = {"0-5":[35, random.uniform(0,5)], "5-10":[19, random.uniform(5,10)], "10-15":[19, random.uniform(10,15)], "15-20":[13, random.uniform(15,20)], "20-25":[3, random.uniform(20,25)], "25-30":[7, random.uniform(25,30)], "30-35":[1, random.uniform(30,35)], "35-40":[2, random.uniform(0,5)], "40-45":[1, random.uniform(0,5)]} 
 		self.lista = []
 		
+		# Em uma lista, para cada par chave-valor do dicionário, o valor da chave é multiplicado por sua probabilidade
+		# Exemplo: chave "20-25", probabilidade 3% = 3 / 100
+		# ["20-25" * 3]
+		# Resultado: ["20-25","20-25","20-25"]
 		for i in range(len(self.probabilidades)):
-			self.lista += [self.probabilidades.keys()[i]] * self.probabilidades.values()[i][0] # concatenação
-		
-		return self.probabilidades[random.choice(self.lista)][1]
+			self.lista += [self.probabilidades.keys()[i]] * self.probabilidades.values()[i][0]
 
-	def chegadaEntidades(self, TEMPO_MEDIO_CHEGADA, i = 0):
-		self.TEMPO_MEDIO_CHEGADA = TEMPO_MEDIO_CHEGADA
-		self.i = i
+		# retorna random.uniform(x, y) # onde x e y é o intervalo. O método random.choice escolhe um valor qualquer na lista
+		return self.probabilidades[random.choice(self.lista)][1] 
+
+	def chegadaClientes(self, contador = 0):
+		self.contador = contador #conta o número de chegadas de entidades
 		
 		while True:
-			self.i += 1
-			yield self.env.timeout(self.tempoChegada())
-			print "\nCliente %d chegou em %.2f" %(self.i, self.env.now)
-			self.env.process(self.atendimento(1, "Cliente %d" %(self.i)))
-			
+			self.contador += 1 #incrementa o contador com uma nova chegada
+			yield self.env.timeout(self.tempoChegada()) #env.timeout causa um atraso de tempo aleatório definido pelo método tempoChegada()
+			print "\nCliente %d chegou em %.2f" %(self.contador, self.env.now) #imprime o valor do contador e o tempo atual de simulação 
+			self.env.process(self.atendimento(1, "Cliente %d" %(self.contador))) # invoca o processo de atendimento dos clientes
+
 	def atendimento(self, TEMPO_MEDIO_ATENDIMENTO, nomeCliente, request=None):
 		self.TEMPO_MEDIO_ATENDIMENTO = TEMPO_MEDIO_ATENDIMENTO
 		self.nomeCliente = nomeCliente
 		self.request = request
+
+		self.request = self.servidor.request() #requisita o uso do recurso
+		yield self.request #aguarda até a liberação do recurso
 		
-		self.request = self.servidor.request()
-		yield self.request
-		
-		print "%.2f %s --> Início do atendimento" %(self.env.now, self.nomeCliente)
+		print "%.2f %s começou a ser atendido" %(self.env.now, self.nomeCliente) # imprime o tempo atual e o início do atendimento
 		
 		yield self.env.timeout(self.TEMPO_MEDIO_ATENDIMENTO)
 		
-		print "%.2f %s --> Término do atendimento" %(self.env.now, self.nomeCliente)
-		self.servidor.release(self.request)
+		print "%.2f %s terminou de ser atendido" %(self.env.now, self.nomeCliente) # imprime o tempo atual e o fim do atendimento
+		yield self.servidor.release(self.request) #libera o recurso
 
-sim = Simulacao(None)
+
+sim = Simulacao(500)
