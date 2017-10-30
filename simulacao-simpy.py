4b11734d85501a2cb906d0bb2258960f9dc1ae40
AndersonBatalha <andersonpbatalha@gmail.com>
2017-10-29 22:46:32 -0200

    Commit inicial

--- simulacao-simpy.py
+++ simulacao-simpy.py
@@ -25,10 +25,10 @@ def tempoChegada(probabilidades = {}, lista = []):
 	# retorna random.uniform(x, y) # onde x e y é o intervalo. O método random.choice escolhe um valor qualquer na lista
 	return probabilidades[random.choice(lista)][1]
 
-def tempoServico(Servidor, probabilidades = {}, lista = []):
+def tempoServico(NServidor, probabilidades = {}, lista = []):
 	#Esta função gera um tempo de servico de acordo com a tabela 2 (ver descrição da atividade)
 
-	# Um dicionário representa com pares no formato chave-valor, onde a chave representa a classe, e o valor é uma lista contendo as probabilidades dos servidores 1 e 2(inteiro), e o método random.uniform, para gerar valores aleatórios de ponto flutuante
+	# Um dicionário representa com pares no formato chave-valor, onde a chave representa a classe, e o valor é uma lista contendo as probabilidades dos NServidores 1 e 2(inteiro), e o método random.uniform, para gerar valores aleatórios de ponto flutuante
 
 	probabilidades = {"9,5-10":[6,5, random.uniform(9.5,10)], "10-10,5":[5,4, random.uniform(10,10.5)], "10,5-11":[23,15, random.uniform(10.5,11)], "11-11,5":[20,16, random.uniform(11,11.5)], "11,5-12":[21,23, random.uniform(11.5,12)], "12-12,5":[12,20, random.uniform(12,12.5)], "12,5-13":[9,10, random.uniform(12.5,13)], "13-13,5":[2,5, random.uniform(13,13.5)], "13,5-14":[1,2, random.uniform(13.5, 14)]}
 
@@ -38,7 +38,7 @@ def tempoServico(Servidor, probabilidades = {}, lista = []):
 	# Resultado: ["9,5-10","9,5-10","9,5-10","9,5-10","9,5-10","9,5-10"]
 
 	for i in range(len(probabilidades)):
-		lista += [probabilidades.keys()[i]] * probabilidades.values()[i][Servidor]
+		lista += [probabilidades.keys()[i]] * probabilidades.values()[i][NServidor]
 
 	random.shuffle(lista) # embaralha a lista para obter um valor mais aleatório possível
 
@@ -49,22 +49,40 @@ def chegadas(env, servidor):
 	contador = 0 # contador de chegadas
 	while True: # executa a função enquanto a simulação estiver ocorrendo
 		contador += 1 # incrementa o contador com uma chegada
-		yield env.timeout(tempoChegada()) # causa um atraso de tempo definido pela funcao tempoChegada()
+		#~ yield env.timeout(tempoChegada()) # causa um atraso de tempo definido pela funcao tempoChegada()
+		yield env.timeout(0.5) # causa um atraso de tempo definido pela funcao tempoChegada()
 		print "\n%.2f\tUsuário %d chegou na fila" %(env.now, contador) # imprime o tempo de chegada
 		env.process(atendimento(env, "Usuario %d" %(contador), servidor)) # executa o processo para atendimento dos clientes
 
-def atendimento(env, nome, servidor):
+def atendimento(env, nome, servidor, usuarios_ativos = [None,None], numeroServidor=0, numeroMedioFila = {}):
+	tamanhoFila = len(servidor.queue)
+	print "Tamanho da fila", tamanhoFila
+	chegada = env.now
 	request = servidor.request() #requisita o uso do servidor
+	# Cria uma lista com duas posições vazias (cada uma correspondente a um servidor)
+	if usuarios_ativos[0] == None and usuarios_ativos[1] == None: # Verifica se ambos os servidores estão desocupados
+		numeroServidor = random.randint(0,1) # escolhe um dos servidores aleatoriamente
+	else: #verifica qual dos servidores está ocupado (1 ou 2)
+		if usuarios_ativos[0] == None and usuarios_ativos[1] != None: 
+			numeroServidor = 0
+		elif usuarios_ativos[0] != None and usuarios_ativos[1] == None:
+			numeroServidor = 1
+	usuarios_ativos[numeroServidor] = request
 	yield request #aguarda até o servidor estar livre
-	print "%.2f\t%s iniciou atendimento" %(env.now, nome) #imprime o início do atendimento
-	yield env.timeout(tempoServico(0))
-	print "%.2f\t%s terminou atendimento" %(env.now, nome) #imprime o fim do atendimento
+	print "%.2f\t%s iniciou atendimento no Servidor %d" %(env.now, nome, numeroServidor + 1) #imprime o início do atendimento
+	#~ yield env.timeout(tempoServico(numeroServidor))
+	yield env.timeout(2.5)
+	print "%.2f\t%s terminou atendimento no Servidor %d" %(env.now, nome, numeroServidor + 1) #imprime o fim do atendimento
+	usuarios_ativos[numeroServidor] = None #desocupa a posição da lista correspondente ao servidor
 	yield servidor.release(request) #libera o recurso
-
+	tempoTotal = env.now - chegada
+	if tamanhoFila not in numeroMedioFila.keys():
+		numeroMedioFila[tamanhoFila] = tempoTotal
+	else:
+		numeroMedioFila[tamanhoFila] += tempoTotal
+	print numeroMedioFila
 
 env = simpy.Environment() #variável que representa o ambiente de simulação criado pelo SimPy
 servidor = simpy.Resource(env, capacity=2) #variavel que representa o recurso a ser ocupado pelos clientes (servidor)
-#~ servidor.users = [None, None]
 env.process(chegadas(env, servidor)) #cria o processo de chegadas de entidade
 env.run(until=100) # executa a simulação por 100 unidades de tempo
-
