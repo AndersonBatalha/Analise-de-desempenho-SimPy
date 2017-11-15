2b14d78d5ef9792d28d9eb40610b4bf5f47b66ac
AndersonBatalha <andersonpbatalha@gmail.com>
2017-11-15 19:27:55 -0200

    Commit inicial

--- simulacao-simpy.py
+++ simulacao-simpy.py
@@ -4,14 +4,16 @@
 import random # gerador de números aleatórios
 import simpy # biblioteca de simulação
 
+contaChegada = 0
+
 class Simulacao(object):
 	def __init__(self, tempoSimulacao, env=None, servidor=None):
 		self.tempoSimulacao = tempoSimulacao
 		self.env = simpy.Environment() #variável que representa o ambiente de simulação criado pelo SimPy
 		self.servidor = simpy.Resource(self.env, capacity=2) #variavel que representa o recurso a ser ocupado pelos clientes (servidor)
-		self.env.process(self.chegadas()) #cria o processo de chegadas de entidade
+		self.env.process(self.chegadasUsuarios()) #cria o processo de chegadas de entidade
 		self.env.run(until=self.tempoSimulacao) # executa a simulação por 100 unidades de tempo
-
+		
 	def tempoChegada(self, probabilidades = {}, lista = []):
 		#Esta função gera um tempo de chegadas de acordo com a tabela 1 (ver descrição da atividade)
 
@@ -53,5 +55,46 @@ class Simulacao(object):
 
 		# retorna random.uniform(x, y) # onde x e y é o intervalo. O método random.choice escolhe um valor qualquer na lista
 		return self.probabilidades[random.choice(self.lista)][2]
+		
+	def chegadasUsuarios(self):
+		self.contaClientes = 0
+		while True:
+			self.contaClientes += 1
+			yield self.env.timeout(self.tempoChegada())
+			print "%.2f\tUsuário %d chegou para ser atendido" %(self.env.now, self.contaClientes)
+			self.env.process(self.atendimento("Usuário %d" %(self.contaClientes)))
 
-S = Simulacao(100)
+	def atendimento(self, n):
+		global contaChegada
+		self.n = n
+		request = self.servidor.request()
+		contaChegada += 1
+		print "%.2f\t%d iniciou atendimento"%(self.env.now, contaChegada)
+		yield request
+		yield self.env.timeout(self.tempoServico(random.randint(0,1)))
+		yield self.servidor.release(request)
+		print "%.2f\t%d terminou atendimento"%(self.env.now, contaChegada)
+ 
+S = Simulacao(75)
+
+def chegadasUsuarios(env, servidor):
+	contaClientes = 0
+	while True:
+		contaClientes += 1
+		yield env.timeout(S.tempoChegada())
+		print "%.2f\tUsuário %d chegou para ser atendido" %(env.now, contaClientes)
+		env.process(atendimento(env, "Usuário %d" %(contaClientes), servidor))
+
+def atendimento(env, n, servidor):
+	request = servidor.request()
+	print "%.2f\t%s iniciou atendimento"%(env.now, n)
+	yield request
+	yield env.timeout(S.tempoServico(random.randint(0,1)))
+	yield servidor.release(request)
+	print "%.2f\t%s terminou atendimento"%(env.now, n)
+
+
+env = simpy.Environment() #variável que representa o ambiente de simulação criado pelo SimPy
+servidor = simpy.Resource(env, capacity=2) #variavel que representa o recurso a ser ocupado pelos clientes (servidor)
+env.process(chegadasUsuarios(env, servidor)) #cria o processo de chegadas de entidade
+#~ env.run(until=S.tempoSimulacao) # executa a simulação por 100 unidades de tempo
