282ceb869b093f2f24c939eab9cb098ff34f9c6c
AndersonBatalha <andersonpbatalha@gmail.com>
2017-11-25 00:06:38 -0200

    Commit inicial

--- simulacao-simpy.py
+++ simulacao-simpy.py
@@ -11,7 +11,7 @@ class Simulacao(object):
 		self.servidor = simpy.Resource(self.env, capacity=2) #variavel que representa o recurso a ser ocupado pelos clientes (servidor)
 		self.env.process(self.chegadas()) #cria o processo de chegadas de entidade
 		self.env.run(until=self.tempoSimulacao) # executa a simulação por 100 unidades de tempo
-
+		
 	def tempoChegada(self, probabilidades = {}, lista = []):
 		#Esta função gera um tempo de chegadas de acordo com a tabela 1 (ver descrição da atividade)
 
@@ -26,7 +26,7 @@ class Simulacao(object):
 		# Resultado: ["20-25","20-25","20-25"]
 
 		for i in range(len(self.probabilidades)):
-				self.lista += [self.probabilidades.keys()[i]] * self.probabilidades.values()[i][0]
+			self.lista += [self.probabilidades.keys()[i]] * self.probabilidades.values()[i][0]
 
 		random.shuffle(self.lista) # embaralha a lista para obter um valor mais aleatório possível
 
@@ -48,75 +48,71 @@ class Simulacao(object):
 		# Resultado: ["9,5-10","9,5-10","9,5-10","9,5-10","9,5-10","9,5-10"]
 
 		for i in range(len(self.probabilidades)):
-				self.lista += [self.probabilidades.keys()[i]] * self.probabilidades.values()[i][self.NServidor]
+			self.lista += [self.probabilidades.keys()[i]] * self.probabilidades.values()[i][self.NServidor]
+		
 		random.shuffle(self.lista) # embaralha a lista para obter um valor mais aleatório possível
 
 		# retorna random.uniform(x, y) # onde x e y é o intervalo. O método random.choice escolhe um valor qualquer na lista
 		return self.probabilidades[random.choice(self.lista)][2]
 
-	def verificaServidorLivre(self, processos_ativos, numeroServidor = 0): #método que verifica qual dos servidores está livre, e retorna para a função atendimento
-		self.processos_ativos = processos_ativos
-		self.numeroServidor = numeroServidor
+	def chegadas(self, contador = 0):
+		self.contador = contador
+		while True: # gera chegadas de entidades enquanto a simulação for executada
+			self.contador += 1
+			yield self.env.timeout(self.tempoChegada()) # gera um atraso de tempo referente a chegada
+			print "\n%.2f\tUsuário %d chegou no sistema" % (self.env.now, self.contador)
+			self.env.process(self.atendimento("Usuário %d" %(self.contador))) # invoca o processo de atendimento
+
+	def atendimento(self, nomeCliente):
+		self.nomeCliente = nomeCliente
+		request = self.servidor.request()
+		yield request
+		print "%.2f\t%s iniciou atendimento" %(self.env.now, self.nomeCliente)
+		yield self.env.timeout(self.tempoServico(random.randint(0,1)))
+		print "%.2f\t%s finalizou atendimento" %(self.env.now, self.nomeCliente)
+		yield self.servidor.release(request) #libera o recurso associado ao servidor
+
+S = Simulacao(150)
+
+class Estatisticas(object):
+	def __init__(self):
+		self.processos_ativos = [None, None]
+		self.numero_servidor = -1
+		self.tempoFila = 0
+		self.n_elementos_fila = 0
+		self.tempos = {}
+		self.tempoOcupacao = 0
+
+	def verificaServidorLivre(self): #método que verifica qual dos servidores está livre, e retorna para a função atendimento
 		# verifica se ambos os servidores estão livres
 		if self.processos_ativos[0] == None and self.processos_ativos[1] == None:
-				self.numeroServidor = random.randint(0,1) #se os servidores estão livres, escolhe um aleatoriamente
+			self.numero_servidor = random.randint(0,1) #se os servidores estão livres, escolhe um aleatoriamente
 		#verifica qual dos servidores está ocupado
 		elif self.processos_ativos[0] == None and self.processos_ativos[1] != None:
-				self.numeroServidor = 0
+			self.numero_servidor = 0
 		elif self.processos_ativos[0] != None and self.processos_ativos[1] == None:
-				self.numeroServidor = 1
-		return self.numeroServidor
+			self.numero_servidor = 1
+		return self.numero_servidor
 
-	def numeroMedioUsuariosFila(self, t, n_elementos_fila, tempoNumeroUsuariosFila = {}):
-		self.t = t
-		self.n_elementos_fila = n_elementos_fila
-		self.tempoNumeroUsuariosFila = tempoNumeroUsuariosFila
-		if self.n_elementos_fila not in self.tempoNumeroUsuariosFila.keys():
-				self.tempoNumeroUsuariosFila[self.n_elementos_fila] = self.t
+	def numeroMedioUsuariosFila(self):
+		if self.n_elementos_fila not in self.tempos.keys():
+			self.tempos[self.n_elementos_fila] = self.t
 		else:
-				self.tempoNumeroUsuariosFila[self.n_elementos_fila] += self.t
-		print "\nTempo em que o numero de elementos na fila permaneceu em determinado estado:\n", self.tempoNumeroUsuariosFila
+			self.tempos[self.n_elementos_fila] += self.t
+		print "\nTempo em que o numero de elementos na fila permaneceu em determinado estado:\n", self.tempos
 
-	def taxaMediaOcupacao(self, tempoOcupado, s, tempos = {}):
-		self.tempoOcupado = tempoOcupado
-		self.s = s
-		self.tempos = tempos
-		if self.s == 0:
+	def taxaMediaOcupacao(self):
+		if self.numero_servidor == 0:
 			if "Servidor 1" not in self.tempos.keys():
 				self.tempos["Servidor 1"] = self.tempoOcupado
 			else:
 				self.tempos["Servidor 1"] += self.tempoOcupado
-		elif self.s == 1:
+		elif self.numero_servidor == 1:
 			if "Servidor 2" not in self.tempos.keys():
 				self.tempos["Servidor 2"] = self.tempoOcupado
 			else:
 				self.tempos["Servidor 2"] += self.tempoOcupado
-		print self.tempos
-
-	def chegadas(self, contador = 0):
-		self.contador = contador
-		while True: # gera chegadas de entidades enquanto a simulação for executada
-				self.contador += 1
-				yield self.env.timeout(self.tempoChegada()) # gera um atraso de tempo referente a chegada
-				print "\n%.2f\tUsuário %d chegou no sistema" % (self.env.now, self.contador)
-				self.env.process(self.atendimento("Usuário %d" %(self.contador))) # invoca o processo de atendimento
-
-	def atendimento(self, nomeCliente, usuarios = [None,None]):
-		self.nomeCliente = nomeCliente
-		self.usuarios = usuarios #cria uma lista com duas posições, que corresponde aos servidores
-		request = self.servidor.request()
-		chegadaFila = self.env.now
-		servidor_livre = self.verificaServidorLivre(self.usuarios)
-		self.usuarios[servidor_livre] = request
-		yield request
-		tempoFila = self.env.now - chegadaFila
-		print "%.2f\t%s iniciou atendimento no Servidor %d" %(self.env.now, self.nomeCliente, servidor_livre + 1)
-		yield self.env.timeout(self.tempoServico(servidor_livre))
-		print "%.2f\t%s finalizou atendimento no Servidor %d" %(self.env.now, self.nomeCliente, servidor_livre + 1)
-		self.usuarios[servidor_livre] = None
-		yield self.servidor.release(request) #libera o recurso associado ao servidor
-		print "Tempo na fila", tempoFila
-		self.numeroMedioUsuariosFila(tempoFila, len(self.servidor.queue))
+		print "Taxa de ocupação dos servidores", self.tempos
 
 
-S = Simulacao(150)
+print Estatisticas().verificaServidorLivre()
